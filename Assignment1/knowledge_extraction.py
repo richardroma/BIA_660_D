0cb03ef11623db32b0283f5697e38dffef28b3ce
Rick <rroma@stevens.edu>
2018-01-30 19:46:01 -0500

    Knowledge Extraction 'chatbot_data.txt'

---
+++ Assignment1/knowledge_extraction.py
@@ -0,0 +1,8 @@
+infile = open('chatbot_data.txt', 'r')
+
+outfile = open('output.txt', 'w')
+
+file_text = infile.read()
+
+print(file_text)
+
--- Assignment1/test2.py
+++ Assignment1/test2.py
@@ -1,6 +1,4 @@
 print("hello world")
-
 a=2
-
 print(a)
 
