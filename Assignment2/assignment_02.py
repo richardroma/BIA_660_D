0f289019065740c5ba593e8994d7622e6a107844
Rick <rroma@stevens.edu>
2018-03-07 17:39:29 -0500

    Assignment #2 - MLB.com Scrape (3/7/18)

--- Assignment2/assignment_02.py
+++ Assignment2/assignment_02.py
@@ -166,6 +166,10 @@ season_type_element = driver.find_element_by_id('sp_hitting_game_type')
 season_type_select = Select(season_type_element)
 season_type_select.select_by_value("'R'")
 
+yank_element = driver.find_element_by_id('sp_hitting_team_id')
+yank_type_select = Select(yank_element)
+yank_type_select.select_by_value("147")
+
 data_div_3 = driver.find_element_by_id('datagrid')
 data_html_3 = data_div_3.get_attribute('innerHTML')
 
@@ -190,3 +194,69 @@ def extract_player_hitting_data(data_element):
 df3 = extract_player_hitting_data(data_div_3)
 
 df3.to_csv('/Users/rickroma/Desktop/Assignment2/Question_3.csv')
+
+#filtered_df3 = df3.sort_values[(df3['AVG']) & (df3['AB'] >= '30')]
+#print(filtered_df3)
+
+
+#Question 4
+
+driver.get('http://www.mlb.com')
+
+stats_header_bar = driver.find_element_by_class_name('megamenu-navbar-overflow__menu-item--stats')
+
+stats_header_bar.click()
+
+stats_line_items = stats_header_bar.find_elements_by_tag_name('li')
+
+stats_line_items[0].click()
+
+hitting_average_element = driver.find_element_by_id('sp_hitting_season')
+hitting_average_select = Select(hitting_average_element)
+hitting_average_select.select_by_value('2015')
+
+season_type_element = driver.find_element_by_id('sp_hitting_game_type')
+season_type_select = Select(season_type_element)
+season_type_select.select_by_value("'R'")
+
+wait = WebDriverWait(driver, 10)
+
+team_ab_stats = wait.until(EC.visibility_of_element_located((By.ID, 'datagrid')))
+
+print('The AB dropdown in the header was loaded successfully. The mouse will move over the element after a short delay')
+normal_delay = random.normalvariate(2, 0.5)
+print('Sleeping for {} seconds'.format(normal_delay))
+time.sleep(normal_delay)
+print('Now moving mouse...')
+ActionChains(driver).move_to_element(team_ab_stats).perform()
+
+team_ab_total = team_ab_stats.find_elements_by_tag_name('th')
+
+team_ab_total[7].click()
+
+data_div_4 = driver.find_element_by_id('datagrid')
+data_html_4 = data_div_4.get_attribute('innerHTML')
+
+def extract_player_ab_data(data_element):
+    data_html_4 = data_element.get_attribute('innerHTML')
+    soup_4 = bs4.BeautifulSoup(data_html_4, 'html5lib')
+
+    column_names = [t.text.replace('▼', ' ').replace('▲', ' ').strip() for t in soup_4.thead.tr.findAll('th')]
+
+    row_lists = []
+    for row in soup_4.tbody.findAll('tr'):
+        row_lists.append([col.text for col in row.findAll('td')])
+
+    df = pd.DataFrame(row_lists, columns=column_names)
+
+    numeric_fields = ['HR']
+    for field in numeric_fields:
+        df[field] = pd.to_numeric(df[field])
+
+    return df
+
+df4 = extract_player_ab_data(data_div_4)
+
+df4.to_csv('/Users/rickroma/Desktop/Assignment2/Question_4.csv')
+
+
